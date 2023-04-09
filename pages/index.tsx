4c0ae709a41d348a3ab045b65af8ecf6bb945df3
prakash.sain <prakash.sain@appinventiv.com>
2023-04-09 20:48:41 +0530

    logo added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import * as React from "react";
+import Container from "@mui/material/Container";
+// import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+// import Link from '../src/Link';
+// import ProTip from '../src/ProTip';
+// import Copyright from '../src/Copyright';
 
 export default function Home() {
   return (
@@ -12,20 +12,21 @@ export default function Home() {
       <Box
         sx={{
           my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Typography variant="h4" component="h1" gutterBottom>
+        <img src="/groovy-dots.gif" />
+        {/* <Typography variant="h4" component="h1" gutterBottom>
           Material UI - Next.js example in TypeScript
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
         <ProTip />
-        <Copyright />
+        <Copyright /> */}
       </Box>
     </Container>
   );
